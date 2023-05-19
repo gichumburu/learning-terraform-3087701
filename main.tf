010c144b39dbe6bfd84348fde39eba7da269ac5d
gichumburu <gichu@mbaruk.com>
2023-05-19 18:52:01 +0300

    2nd set of fixes

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -51,7 +51,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "eggress"
@@ -60,5 +60,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
