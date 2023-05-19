7c7d90816eae2b57dfdfbf0d8492e69bc33c4ace
gichumburu <gichu@mbaruk.com>
2023-05-19 18:54:09 +0300

    update security group

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group_rule.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "UhoroWaku-tena_Muno"
@@ -54,7 +54,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "eggress"
+  type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
