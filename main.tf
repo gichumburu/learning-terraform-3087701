8e316797770bc88f172e95e71346fa5d84dda8c7
gichumburu <gichu@mbaruk.com>
2023-05-21 12:09:54 +0300

    removed alb logs

--- main.tf
+++ main.tf
@@ -58,7 +58,10 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-/*
+  /* access_logs = {
+    bucket = "my-alb-logs"
+  }*/
+
   target_groups = [
     {
       name_prefix      = "blog"
@@ -73,7 +76,7 @@ module "blog_alb" {
         
       }
     }
-  ] */
+  ]
 
 
   http_tcp_listeners = [
